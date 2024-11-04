0c2fcef5ce4752e06e85a17bc3a045e95364a908
unknown <hyder1@gmail.com>
2024-11-04 09:18:25 -0500

    updates

--- src/hooks/useDeadline.ts
+++ src/hooks/useDeadline.ts
@@ -2,9 +2,9 @@ import dayjs from "dayjs";
 
 export const useDeadline = () => {
     const deadline = dayjs("11/4/2024");
-    const date = dayjs().isBefore(deadline)
-        ? deadline.format("MMMM DD, YYYY")
-        : "December 31, 2024";
+    const date = dayjs().isAfter('11/5/2024')
+        ? "November 18, 2024"
+        : deadline.format("MMMM DD, YYYY")
 
     return { date }
 }
