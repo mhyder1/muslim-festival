ea1a3e7644782f7321b989af486377ad3bfc738a
unknown <hyder1@gmail.com>
2025-03-20 12:40:00 -0400

    updates

--- src/components/contest/Contest.jsx
+++ src/components/contest/Contest.jsx
@@ -2,7 +2,7 @@ const Contest = () => {
   return (
     <div
       class="page container page-flex"
-      //   style={{ minHeight: "calc(100vh - 117px)" }}
+      style={{ minHeight: "calc(100vh - 117px)" }}
     >
       <div class="page-container">
         <h1
@@ -29,20 +29,17 @@ const Contest = () => {
               closer to the day of event so check your email.
             </li>
           </ul>
-          If interested in participating in the contest, please complete your
-          registration form before <strong>Monday, March 31st</strong>.
         </p>
         <h4>Prizes:</h4>
         <p>CHILDREN 1st: Tablet, 2nd: Bike, 3rd: $25 gift card</p>
         <p>TEENS 1st: $100.00, 2nd: $50.00, 3rd: $25.00</p>
         <p>ADULTS 1st: $150.00, 2nd: $75.00, 3rd: $50.00</p>
-        <a
-          href="https://forms.gle/xtmqvBbp2mkb4wtW9"
-          target="_blank"
-          class="btn btn-primary btn-xl rounded-pill mt-1 mb-4"
-        >
-          Register Now
-        </a>
+        <p>
+          <strong>
+            Registration for the adhan contest is closed / full. See you at the
+            festival!
+          </strong>
+        </p>
       </div>
     </div>
   );
